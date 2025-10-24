d1df255f2dccb28e94f7b330f79a4c1969011818
MishkatMukit <mahabubmishkat8@gmail.com>
2025-10-24 23:46:09 +0600

    navbar added

--- src/assets/Components/Navbar/Navbar.jsx
+++ src/assets/Components/Navbar/Navbar.jsx
@@ -26,24 +26,27 @@ const Navbar = () => {
                     <a className="text-xl"><span className='text-[#003EA4] font-medium'>Auction</span><span className='text-[#FFD337] font-bold'>Gallery</span></a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
+                    <ul className="menu menu-horizontal px-1 font-medium">
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Auction</a></li>
+                        <li><a href="">Categories</a></li>
+                        <li><a href="">How to work</a></li>
                     </ul>
                 </div>
-                <div className="navbar-end">
-                    <a className="btn">Button</a>
+                <div className="navbar-end flex gap-5">
+                    <div className="indicator">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /> </svg>
+                        <span className="badge badge-xs badge-primary indicator-item"></span>
+                    </div>
+
+                    <div className="w-10 ">
+                        <img className='rounded-full'
+                            alt="Tailwind CSS Navbar component"
+                            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                    </div>
                 </div>
             </div>
+
         </div>
     );
 };
